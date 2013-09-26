38d541b88fb1b3db408197edec6f0bbc9969daaf
Joel Alves Costa Filho <joelfilho93@gmail.com>
2013-09-26 19:51:23 -0300

    Adicionando mudanças

---
+++ Pessoa/src/pessoa/PessoaFisica.java
@@ -0,0 +1,33 @@
+package pessoa;
+
+public class PessoaFisica extends Pessoa {
+
+	private String sobrenome;
+	private String cpf;
+	private String cargo;
+	
+	public PessoaFisica(String cpf){
+		this.cpf = cpf;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+
+}
