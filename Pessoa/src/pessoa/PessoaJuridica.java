ef62a87c7bcf92d7915cddbe5faa3ae054c53b1d
Joel Alves Costa Filho <joelfilho93@gmail.com>
2013-09-26 19:52:52 -0300

    Adicionando mudanças

---
+++ Pessoa/src/pessoa/PessoaJuridica.java
@@ -0,0 +1,29 @@
+package pessoa;
+
+public class PessoaJuridica extends Pessoa {
+
+
+		
+		private String cnpj;
+		private String razaoSocial;
+		
+		public PessoaJuridica(String cnpj){
+			this.cnpj = cnpj;
+		}
+		
+		public String getCnpj() {
+			return cnpj;
+		}
+		public void setCnpj(String cnpj) {
+			this.cnpj = cnpj;
+		}
+		public String getRazaoSocial() {
+			return razaoSocial;
+		}
+		public void setRazaoSocial(String razaoSocial) {
+			this.razaoSocial = razaoSocial;
+		}
+
+	
+
+}
